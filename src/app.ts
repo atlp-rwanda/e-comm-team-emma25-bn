7cc3ebb6cdf6b065b3b80164cc54c2b353c607ca
Rukundojoseph <104127964+Rukundojoseph@users.noreply.github.com>
2023-02-01 07:09:37 -0900

    feat: connected to the database

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,19 @@
 import express from 'express'
 import { config } from 'dotenv'
+import connectdb from './db/database'
 const app = express()
 config()
+//middleware section
+app.use(express.json())
 
 console.log('Hello Team emma This backend API')
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}`));
+
+
+/*called the database connection below */
+connectdb().then( 
+    ()=>{
+app.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}`))
+}
+)
