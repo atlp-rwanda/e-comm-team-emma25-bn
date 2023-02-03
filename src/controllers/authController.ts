4c897073544d3482d12bf3683ffcde0762a3790a
KABANOFESTO <kabanofesto1@gmail.com>
2023-02-03 10:22:05 +0200

    (ch-eslint):Actually Eslint is a linting tool help us to identify and fix problems in our code -Eslinting help us to check our potential errors -qualify our code by enforcing a consistent coding style and best practices. -ESLint can be integrated with a variety of development tools, such as text editors, build tools, and continuous integration pipelines.

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,77 +1,78 @@
-import USER from "../models/User";
-import {Request , Response} from "express"
+// import USER from "../models/User";
+// import {Request , Response} from "express"
 
-/* this class hold functions for authentication */
-class auth {
-    static async signup(req: Request ,res:Response){
-        //defined the type of user to be created 
-        type newuser ={
-            firstName: string,
-            lastName : string,
-            email: string,
-            password: string,
-        }
-const userReq: newuser={
-    firstName: req.body.firstName ,
-    lastName: req.body.lastName ,
-    email:req.body.email ,
-    password:req.body.password,
-}
-        try {
-            const user = await USER.create(userReq)
-            if(user){
-                res.status(200).json({
-                    statusCode: 200,
-                    message: "created account succesfully"
-                })              
-            }
-            // else{
-            //     res.status(400).json({
-            //         statusCode: 200,
-            //         message: "Failed to create user",                  
-            //     })                    
-            // }
-        } catch (error: any) {
-            res.status(400).json({
-                statusCode: 400,
-                message: "Failed to create user",
-                error: error.message                  
-            })            
-        }      
+// /* this class hold functions for authentication */
+// class auth {
+//     static async signup(req: Request ,res:Response){
+//         //defined the type of user to be created 
+//         type newuser ={
+//             firstName: string,
+//             lastName : string,
+//             email: string,
+//             password: string,
+//         }
+// const userReq: newuser={
+//     firstName: req.body.firstName ,
+//     lastName: req.body.lastName ,
+//     email:req.body.email ,
+//     password:req.body.password,
+// }
+//         try {
+//             const user = await USER.create(userReq)
+//             if(user){
+//                 res.status(200).json({
+//                     statusCode: 200,
+//                     message: "created account succesfully"
+//                 })              
+//             }
+//             // else{
+//             //     res.status(400).json({
+//             //         statusCode: 200,
+//             //         message: "Failed to create user",                  
+//             //     })                    
+//             // }
+//         } 
+//         catch (error: any) {
+//             res.status(400).json({
+//                 statusCode: 400,
+//                 message: "Failed to create user",
+//                 error: error.message                  
+//             })            
+//         }      
 
-    }
-    static async getAlluser(req: Request ,res: Response){
-        try {           
-            const users : object = await USER.findAll({attributes: { exclude: ['password'] }})
-            res.status(200).json({
-                statuscode: 200,
-                users
-            })            
-        } catch (error: any) {
-            res.status(400).json({
-                statusCode: 400,
-                "message" : error.message
-            })
-        }
-    }
-    /*this delete user function is not protected it is created just for the project setup and testing*/
-    static async deleteUser(req: Request,res:Response){
-        const userid: string = req.params.id 
-        try {
-            await USER.destroy({where : {id : userid}})          
-            res.status(200).json({
-                statusCode: 200,
-                "message" : `deleted user with id ${userid}`
-            })
+//     }
+//     static async getAlluser(req: Request ,res: Response){
+//         try {           
+//             const users : object = await USER.findAll({attributes: { exclude: ['password'] }})
+//             res.status(200).json({
+//                 statuscode: 200,
+//                 users
+//             })            
+//         } catch (error: any) {
+//             res.status(400).json({
+//                 statusCode: 400,
+//                 "message" : error.message
+//             })
+//         }
+//     }
+//     /*this delete user function is not protected it is created just for the project setup and testing*/
+//     static async deleteUser(req: Request,res:Response){
+//         const userid: string = req.params.id 
+//         try {
+//             await USER.destroy({where : {id : userid}})          
+//             res.status(200).json({
+//                 statusCode: 200,
+//                 "message" : `deleted user with id ${userid}`
+//             })
               
-        } catch (error: any) {
-            res.status(400).json({
-                statusCode: 400,
-                "message" : error.message
-            })
+//         } catch (error: any) {
+//             res.status(400).json({
+//                 statusCode: 400,
+//                 "message" : error.message
+//             })
 
             
-        }
-    }
-}
-export default auth
+//         }
+//     }
+// }
+// export default auth
