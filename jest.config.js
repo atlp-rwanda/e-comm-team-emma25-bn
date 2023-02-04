ba97ffb32abbeced571543af0552f1779877402a
Jean Damascene Uwingeneye <MacbookPro@Jeans-MacBook-Pro.local>
2023-02-04 18:44:29 +0200

    test(jest) add and configure jest testing framework

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  coverageReporters: ['text', 'html'],
+}
