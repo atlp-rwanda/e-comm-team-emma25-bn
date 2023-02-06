4eb40949471bfd234b78d8fa51b5e53685d98fa9
Rukundojoseph <104127964+Rukundojoseph@users.noreply.github.com>
2023-02-06 10:37:23 -0900

    fix: fix any error

--- src/routes/authroutes.ts
+++ src/routes/authroutes.ts
@@ -6,7 +6,7 @@ const router = Router()
 
 router.post('/signup', auth.signup)
 router.get('/users', auth.getAlluser)
-router.delete('/delete/:id', auth.deleteUser)
+router.delete('/delete/:id', auth.deleteUser) 
 /* this delete user route is not protected it is just for testing and setting up the project*/
 
-export default router 
+export default router  
