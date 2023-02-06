2fb2a0b13c1576cbc68fbb980389744ad9589b77
TresorRw <alaintresorcyusa683@gmail.com>
2023-02-06 21:47:42 +0200

    Jest config file

---
+++ src/jest.config.ts
@@ -0,0 +1,12 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  coverageReporters: ['lcov', 'text'],
+  coverageDirectory: 'coverage',
+}
