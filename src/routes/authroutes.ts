f577616624945cef6364c32a53749efff5e94a85
TresorRw <alaintresorcyusa683@gmail.com>
2023-02-07 09:17:08 +0200

    Re-structured the route for verifying the OTP
    use of url params as verify/:phone/:code

--- src/routes/authroutes.ts
+++ src/routes/authroutes.ts
@@ -8,7 +8,7 @@ router.post('/signup', auth.signup)
 router.get('/users', auth.getAlluser)
 router.delete('/delete/:id', auth.deleteUser)
 router.get('/sendcode/:phone', auth.sendCode)
-router.get('/verify/:phone', auth.verify2FA)
+router.get('/verify/:phone/:code', auth.verify2FA)
 /* this delete user route is not protected it is just for testing and setting up the project*/
 
 export default router
