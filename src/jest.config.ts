0b4ae2cef4bc8b5235b3faf31ee7e8699f8a54e7
Jean Damascene Uwingeneye <jeandamas85@gmail.com>
2023-02-08 10:27:23 +0200

    update jest config

--- src/jest.config.ts
+++ src/jest.config.ts
@@ -5,16 +5,9 @@ export default {
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
-  coverageThreshold: {
-    global: {
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
-  },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   coverageReporters: ['lcov', 'text'],
-  collectCoverage: true,
   coverageDirectory: '../coverage',
+  collectCoverage: true,
 }
