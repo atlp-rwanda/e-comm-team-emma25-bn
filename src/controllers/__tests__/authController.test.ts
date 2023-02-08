af6c523ca986c43bf24f65a310eb6f13ebd2a1cc
Jean Damascene Uwingeneye <jeandamas85@gmail.com>
2023-02-08 09:49:24 +0200

    test: logout functionality

--- src/controllers/__tests__/authController.test.ts
+++ src/controllers/__tests__/authController.test.ts
@@ -1,6 +1,19 @@
 // TODO replace the test below with your tests
 import {add, multiply} from '../../totest'
 
+import request from 'supertest'
+import app from '../../app'
+// import auth from '../authController'
+
+describe('Test the logout route', () => {
+  test('It should logout the user and return status 200 and message "Logged out"', async () => {
+    const response = await request(app).get('/logout')
+
+    expect(response.statusCode).toBe(200)
+    expect(response.body).toEqual({status: 200, message: 'Logged out'})
+  })
+})
+
 describe('Math functions', () => {
   it('should multiply 5 by 3', () => {
     const result = multiply(5, 3)
