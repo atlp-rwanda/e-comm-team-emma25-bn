71299bb4c391ecd19187fab46cea559d465fd11f
rachelumwari <umwarirachel13@gmail.com>
2023-02-08 14:46:55 +0200

    added docker compose and docker file

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,26 @@
 import express, {Application} from 'express'
+import passport from 'passport'
+import session from 'express-session'
 import {config} from 'dotenv'
 import swaggerDocs from './docs/swagger'
 import connectdb from './db/database'
 import authRoutes from './routes/authroutes'
 
 const app: Application = express()
+import './config/googlePassport.config'
 
 config()
 //middleware section
+app.use(
+  session({
+    secret: `process.env.SECRET`,
+    resave: false,
+    saveUninitialized: true,
+  }),
+)
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use(express.json())
 
 app.use(authRoutes)
@@ -23,4 +36,4 @@ connectdb().then(() => {
   // change this to just port in case someone is listening from 127.0.0.1 instead of localhost
 })
 
-export default app
+export default app 
---
+++ src/config/googlePassport.config.ts
@@ -0,0 +1,17 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth2").Strategy;
+require('dotenv').config();
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: process.env.CallBackURL,
+    passReqToCallBack: true,
+}, function (request, accessToken, refreshToken, profile, done) {
+    return done(null, profile);
+}));
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
