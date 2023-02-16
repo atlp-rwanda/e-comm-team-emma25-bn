0718219339117bcb7054dd11f1134ef9a9644882
Jean Damascene Uwingeneye <jeandamas85@gmail.com>
2023-02-16 14:59:04 +0200

    TODO

--- src/routes/authroutes.ts
+++ src/routes/authroutes.ts
@@ -44,6 +44,7 @@ router.get('/sendcode/:phone', auth.sendCode)
 router.get('/verify/:phone/:code', auth.verify2FA)
 // router.post('/logout', auth.logout)
 router.post('/logout', auth.logout)
+// TODO PROTECT THE ROUTE
 router.post('/authorize', auth.authorize)
 
 export default router
