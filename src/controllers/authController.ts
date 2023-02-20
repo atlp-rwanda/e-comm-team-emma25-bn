01b0028ddb98335eb0a7d7edf1f5bcd82f47034d
Jean Damascene Uwingeneye <jeandamas85@gmail.com>
2023-02-20 15:50:11 +0200

    Fix: max password from 6 to 10 Fix: Duplicate identifier

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,11 +1,10 @@
 import USER from '../models/User'
 
-import { Request, Response } from 'express'
-import { Twilio } from 'twilio'
-import {  encode  } from '../helper/jwtTokenize'
-import bcrypt from 'bcrypt'
+import {Request, Response} from 'express'
+import {Twilio} from 'twilio'
+import {encode} from '../helper/jwtTokenize'
 
-import { config } from 'dotenv'
+import {config} from 'dotenv'
 import session from 'express-session'
 import connectRedis from 'connect-redis'
 import {createClient} from 'redis'
@@ -13,8 +12,7 @@ import Redis from 'ioredis'
 import bcrypt from 'bcrypt'
 
 const RedisStore = connectRedis(session)
-import {  object  } from 'joi'
-import { object } from 'joi'
+import {object} from 'joi'
 config()
 
 const account_sid = process.env.TWILIO_ACCOUNT_SID
@@ -24,16 +22,15 @@ const service_sid = process.env.TWILIO_SERVICE_SID
 /* this class hold functions for authentication */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 class auth {
-
   static sendCode(req: Request, res: Response) {
     const userPhone: string = req.params.phone
     if (account_sid && authToken && service_sid) {
       const Client = new Twilio(account_sid, authToken)
       Client.verify.v2
         .services(service_sid)
-        .verifications.create({ to: userPhone, channel: 'sms' })
+        .verifications.create({to: userPhone, channel: 'sms'})
         .then((resp) => {
-          res.status(200).json({ message: 'Success sent', resp })
+          res.status(200).json({message: 'Success sent', resp})
         })
         .catch((err) => {
           res.status(400).json(err)
@@ -49,9 +46,9 @@ class auth {
       const Client = new Twilio(account_sid, authToken)
       Client.verify.v2
         .services(service_sid)
-        .verificationChecks.create({ to: userPhone, code: userCode })
+        .verificationChecks.create({to: userPhone, code: userCode})
         .then((resp) => {
-          res.status(200).json({ message: 'Success sent', resp })
+          res.status(200).json({message: 'Success sent', resp})
         })
         .catch((err) => {
           res.status(400).json(err)
@@ -75,10 +72,10 @@ class auth {
   static async signup(req: Request, res: Response) {
     try {
       // await USER.drop()
-      const { firstName, lastName, email, role, password } = req.body
+      const {firstName, lastName, email, role, password} = req.body
       //   const hash = await bcrypt.hashSync(password, 10)
       const checkUser = await USER.findOne({
-        where: { email: email },
+        where: {email: email},
       })
       if (checkUser) {
         return res.status(400).json({
@@ -102,13 +99,13 @@ class auth {
           password,
         })
         const user = await USER.findOne({
-          where: { email: email },
+          where: {email: email},
           attributes: ['id', 'firstName', 'lastName', 'email', 'role'],
         })
         res.status(200).json({
           status: 200,
           message: 'account created successfully',
-          token: encode({ id: createData.id, email: createData.email }),
+          token: encode({id: createData.id, email: createData.email}),
         })
       }
     } catch (error: any) {
@@ -122,13 +119,20 @@ class auth {
 
   static async Login(req: Request, res: Response) {
     try {
-      const { email, password } = req.body
+      const {email, password} = req.body
       const findUser = await USER.findOne({
-        where: { email: email },
-        attributes: ['id', 'firstName', 'lastName', 'email', 'role', 'password'],
+        where: {email: email},
+        attributes: [
+          'id',
+          'firstName',
+          'lastName',
+          'email',
+          'role',
+          'password',
+        ],
       })
       if (!findUser) {
-        res.status(404).json({ message: "User not found" })
+        res.status(404).json({message: 'User not found'})
       } else {
         const dbPassword = findUser.dataValues.password
         const decreptedPassword = await bcrypt.compare(password, dbPassword)
@@ -138,16 +142,19 @@ class auth {
             stastus: 200,
             message: 'Login succefull ',
             data: findUser,
-            token: encode({ id: findUser.dataValues.id, email: findUser.dataValues.email, role: findUser.dataValues.role })
+            token: encode({
+              id: findUser.dataValues.id,
+              email: findUser.dataValues.email,
+              role: findUser.dataValues.role,
+            }),
           })
         } else {
           res.status(400).json({
             stastus: 400,
             message: 'Wrong password',
-          })  
+          })
         }
       }
-
     } catch (error: any) {
       res.status(500).json({
         stastus: 500,
@@ -156,11 +163,10 @@ class auth {
     }
   }
 
-
   static async getAlluser(req: Request, res: Response) {
     try {
       const users: object = await USER.findAll({
-        attributes: { exclude: ['password'] },
+        attributes: {exclude: ['password']},
       })
       res.status(200).json({
         statuscode: 200,
@@ -177,7 +183,7 @@ class auth {
   static async deleteUser(req: Request, res: Response) {
     const userid: string = req.params.id
     try {
-      await USER.destroy({ where: { id: userid } })
+      await USER.destroy({where: {id: userid}})
       res.status(200).json({
         statusCode: 200,
         message: `deleted user with id ${userid}`,
