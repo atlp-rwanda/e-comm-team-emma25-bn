de53c01f50666464745aec058fc5889a3889904d
Jean Damascene Uwingeneye <jeandamas85@gmail.com>
2023-02-24 16:00:27 +0200

    Fix: reslove migration

--- src/db/models/Role.ts
+++ src/db/models/Role.ts
@@ -1,41 +1,18 @@
-'use strict'
 import {sequelizedb} from '../database'
-import {Model} from 'sequelize'
-// const {
-//   Model
-// } = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Role extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Role.init(
-    {
-      name: {
-        type: DataTypes.STRING,
-        unique: true,
-        allowNull: false,
-        set(value: string) {
-          this.setDataValue('name', value.toLowerCase())
-        },
-      },
-      description: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
+import {DataTypes} from 'sequelize'
+const ROLE = sequelizedb.define('user', {
+  name: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+    set(value: string) {
+      this.setDataValue('name', value.toLowerCase())
     },
-    {
-      sequelize,
-      tableName: 'roles',
-      underscored: true,
-    },
-  )
-
-  return Role
-}
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+})
+ROLE.sync()
+export default ROLE
