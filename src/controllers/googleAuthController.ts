aae9851b39347755cbe3e03e690a0520b35683d5
KABANOFESTO <kabanofesto1@gmail.com>
2023-02-24 21:50:47 +0200

    googleAuth

---
+++ src/controllers/googleAuthController.ts
@@ -0,0 +1,50 @@
+import User from '../models/User'
+import bcrypyt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import {randomUUID} from 'crypto'
+
+class GoogleController {
+  static async googleAuth(req: any, res: any) {
+    try {
+      await User.findOne({
+        where: {
+          email: req.user.email,
+        },
+      }).then(async (result: any) => {
+        if (result) {
+          res.status(200).json({
+            success: true,
+            user: {
+              id: result.id,
+              firstName: result.firstName,
+              lastName: result.lastName,
+              email: result.email,
+              token: generateToken(result),
+            },
+          })
+        } else {
+          const salt = await bcrypyt.genSalt(10)
+          const rand = randomUUID()
+          const hashpassword = await bcrypyt.hash(generateToken(rand), salt)
+          const user = await User.create({
+            firstName: req.user.name.givenName,
+            lastName: req.user.name.familyName,
+            email: req.user.email,
+            password: hashpassword,
+          })
+          await user.save()
+
+          return res.status(200).json({user: user})
+        }
+      })
+      return res
+    } catch (error) {
+      return res.status(500).json({status: 500, error: error})
+    }
+  }
+}
+
+const generateToken = (user) => {
+  return jwt.sign({user}, 'my-token-secret', {expiresIn: '30d'})
+}
+export default GoogleController
--- src/middlewares/verifyToken.ts
+++ src/middlewares/verifyToken.ts
@@ -1,57 +1,54 @@
-import { Request,Response,NextFunction } from 'express';
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-import { RequestHandler } from 'express';
-dotenv.config();
+import {Request, Response, NextFunction} from 'express'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+import {RequestHandler} from 'express'
+dotenv.config()
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 interface User {
-  id: number; 
-  email: string;
-  role: string;
+  id: number
+  email: string
+  role: string
 }
 export interface CustomRequest extends Request {
   users?: User;
   [key: string]: any;
 }
 
-const verifyToken: RequestHandler<CustomRequest> =async(req :CustomRequest, res:Response, next: NextFunction)=>{
-try {
-
-  const token=req.headers['authorization'] as string
-  if(token){
-  const auth = token.split(" ")[1]
-  // const token=authHeader && authHeader.split('')[1]
-  if (!auth) {
-      return res.status(403).send("A token is required for authentication");
-    }
-    try {
-      const decoded: any = jwt.verify(auth, process.env.JWT_SECRET as string);
-      req.user = {
-        id: decoded.id,        
-        email: decoded.email,
-        role: decoded.role
+const verifyToken: RequestHandler<CustomRequest> = async (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const token = req.headers['authorization'] as string
+    if (token) {
+      const auth = token.split(' ')[1]
+      // const token=authHeader && authHeader.split('')[1]
+      if (!auth) {
+        return res.status(403).send('A token is required for authentication')
       }
-      next()
-      
-        ;
-    } catch (err) {
-     
+      try {
+        const decoded: any = jwt.verify(auth, process.env.JWT_SECRET as string)
+        req.user = {
+          id: decoded.id,
+          email: decoded.email,
+          role: decoded.role,
+        }
+        next()
+      } catch (err) {
         console.log(err)
-    }}else{
+      }
+    } else {
       res.status(401).json({
         statusCode: 401,
-        message: "you are not logged"
+        message: 'you are not logged',
       })
     }
-    
-  
-} catch (error) {
-  return res.status(403).send("Token not provided");
-  
+  } catch (error) {
+    return res.status(403).send('Token not provided')
+  }
 }
 
-}
-
-export default verifyToken;
+export default verifyToken
