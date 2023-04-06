014d482012fb8610b6a5242bbf75d7bb36919a5a
Rukundojoseph <104127964+Rukundojoseph@users.noreply.github.com>
2023-04-06 01:06:10 -0800

    feat: protect route

--- src/routes/authroutes.ts
+++ src/routes/authroutes.ts
@@ -6,6 +6,7 @@ import GoogleController from "../controllers/googleAuthController";
 import resetpass from "../controllers/resetcontrollers";
 import Stripe from 'stripe'
 import verifyToken from "../middlewares/verifyToken";
+import { roleAuthorization } from "../middlewares/role.middleware";
 
 
 const stripe = new Stripe (process.env.STRIPE_SECRET as string, {
@@ -19,7 +20,7 @@ router.post('/login', auth.Login)
 router.post('/signup', signupValidation, auth.signup)
 router.post('/login', auth.Login)
 router.get('/users', auth.getAlluser)
-router.delete('/delete/:id', auth.deleteUser)
+router.delete('/delete/:id',roleAuthorization(["admin"]), auth.deleteUser)
 router.get('/sendcode/:phone', auth.sendCode)
 router.get('/verify/:phone/:code', auth.verify2FA)
 router.get('/verify/:code', verifyToken, auth.verify2FA)
