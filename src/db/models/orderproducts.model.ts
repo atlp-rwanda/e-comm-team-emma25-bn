881e07e20b498f46c009d24d6666f903c5332538
fabrice-rw <fabricenzayisenga@gmail.com>
2023-05-04 17:59:49 +0200

    orderstatus.routes.ts spell change

--- src/db/models/orderproducts.model.ts
+++ src/db/models/orderproducts.model.ts
@@ -1,47 +1,47 @@
-import {  DataTypes } from 'sequelize';
+import { DataTypes } from 'sequelize';
 import { sequelizedb } from "../database"
 import Order from './order.model';
 import Product from './Product';
 
-  
-  const  OrderProduct = sequelizedb.define( "orderProducts" ,
-    {      
-      orderProductID:{
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      Orderid: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      price: {
-        type: DataTypes.DECIMAL,
-        allowNull: false,
-      },
-      productName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      productId: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      productQuantity:{
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        defaultValue: 1,
-      }
+
+const OrderProduct = sequelizedb.define("orderProducts",
+  {
+    orderProductID: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    {
-      tableName: 'orderproducts'      
+    Orderid: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-  );
-  
-  OrderProduct.belongsTo(Order);
-  Order.hasMany(OrderProduct);  
-  OrderProduct.belongsTo(Product);
-  Product.hasMany(OrderProduct);
-  OrderProduct.sync()
-  
-  export default OrderProduct;
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    productName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    productId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    productQuantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1,
+    }
+  },
+  {
+    tableName: 'orderproducts'
+  },
+);
+
+OrderProduct.belongsTo(Order);
+Order.hasMany(OrderProduct);
+OrderProduct.belongsTo(Product);
+Product.hasMany(OrderProduct, { foreignKey: 'Orderid' });
+OrderProduct.sync()
+
+export default OrderProduct;
